4294e6e17fb1bceaaa59fbe42a58b9f92427fb1e
rjpacket <jimbo922@163.com>
2018-11-23 14:46:58 +0800

    不抖动的ScrollView

--- app/src/main/java/com/rjp/fastframework/MainActivity.java
+++ app/src/main/java/com/rjp/fastframework/MainActivity.java
@@ -3,18 +3,21 @@ package com.rjp.fastframework;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
-import com.rjp.expandframework.utils.ActivityUtil;
-
-import static com.rjp.expandframework.activitys.ForResultActivity.TRANS_BUNDLE;
+import static android.view.View.VISIBLE;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvMsg;
     private Context mContext;
+    private TextView tv1;
+    private TextView tv2;
+    private ScrollView scrollView;
 
     public static void trendTo(Context mContext) {
         Intent intent = new Intent(mContext, MainActivity.class);
@@ -28,34 +31,45 @@ public class MainActivity extends AppCompatActivity {
 
         mContext = this;
 
-        tvMsg = (TextView) findViewById(R.id.tv_msg);
+        scrollView = findViewById(R.id.scrollView);
 
-        tvMsg.setOnClickListener(new View.OnClickListener() {
+        tv1 = findViewById(R.id.tv_1);
+        tv2 = findViewById(R.id.tv_2);
+
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("name", "任金鹏");
-                intent.putExtra(TRANS_BUNDLE, bundle);
-                ActivityUtil.startActivityForResult(intent, 10086, new ActivityUtil.OnActivityForResultListener() {
-                    @Override
-                    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-                        if(data != null) {
-                            tvMsg.setText(data.getStringExtra("age"));
-                        }
+            public void run() {
+               tv1.setVisibility(VISIBLE);
+               tv2.setVisibility(VISIBLE);
+            }
+        }, 3000);
+
+        tv1.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if(tv1.getVisibility() == VISIBLE){
+                    int scrollY = scrollView.getScrollY();
+                    int measuredHeight = tv1.getMeasuredHeight();
+                    if(tv1.getBottom() - measuredHeight < scrollY) {
+                        scrollView.scrollTo(0, scrollY + measuredHeight);
                     }
-                });
+                }
             }
         });
 
-//        ImageView ivImage = findViewById(R.id.iv_image);
-//        ImageLoaderUtil.load(this, "http://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg", ivImage, R.drawable.ic_launcher_background);
-
-        String packageName = getApplication().getPackageName();
-        tvMsg.setText(
-                "包名是：" + packageName + "\n" +
-                "版本号是：" + BuildConfig.VERSION_NAME + "\n" +
-                "类型是：" + BuildConfig.BUILD_TYPE + "\n"
-        );
+        tv2.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if(tv2.getVisibility() == VISIBLE){
+                    int scrollY = scrollView.getScrollY();
+                    int measuredHeight = tv2.getMeasuredHeight();
+                    if(tv2.getBottom() - measuredHeight < scrollY) {
+                        scrollView.scrollTo(0, scrollY + measuredHeight);
+                    }
+                }
+            }
+        });
     }
+
+    private Handler handler = new Handler();
 }
