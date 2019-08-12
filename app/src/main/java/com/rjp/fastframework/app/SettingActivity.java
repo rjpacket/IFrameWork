66e0c0ff1c48fe665d351a4e8f6feb60a5760094
rjp <jimbo922@163.com>
2019-08-12 08:57:13 +0800

    update 456

---
+++ app/src/main/java/com/rjp/fastframework/app/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.rjp.fastframework.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.rjp.fastframework.R;
+
+public class SettingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
