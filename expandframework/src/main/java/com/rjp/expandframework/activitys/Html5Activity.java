81950d77dd3a485c61ceb08bf201d8ca3551a4e9
jinpeng.ren <jinpeng.ren@11bee.com>
2020-02-19 14:57:02 +0800

    update

--- expandframework/src/main/java/com/rjp/expandframework/activitys/Html5Activity.java
+++ expandframework/src/main/java/com/rjp/expandframework/activitys/Html5Activity.java
@@ -1,19 +1,34 @@
 package com.rjp.expandframework.activitys;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Message;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.*;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 import com.rjp.expandframework.R;
+import com.rjp.expandframework.apm.cpu.FileUtils;
+import com.rjp.expandframework.interfaces.PermissionCallback;
+import com.rjp.expandframework.utils.FileUtil;
+import com.rjp.expandframework.utils.PermissionUtil;
+
+import java.io.File;
+import java.util.List;
 
 public class Html5Activity extends AppCompatActivity {
 
@@ -21,11 +36,14 @@ public class Html5Activity extends AppCompatActivity {
 
     private LinearLayout mLayout;
     private WebView mWebView;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+
+        mContext = this;
 
         Bundle bundle = getIntent().getBundleExtra("bundle");
         mUrl = bundle.getString("url");
@@ -129,6 +147,11 @@ public class Html5Activity extends AppCompatActivity {
          */
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Uri uri = Uri.parse(url);
+            if (!("http".equalsIgnoreCase(uri.getScheme()) || "https".equalsIgnoreCase(uri.getScheme()))) {
+                mContext.startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                return true;
+            }
             view.loadUrl(url);
             return true;
         }
@@ -164,7 +187,14 @@ public class Html5Activity extends AppCompatActivity {
         }
     };
 
+    private ValueCallback mUploadMessage;
+    private ValueCallback<Uri[]> mUploadMessages;
+    public static final int FILE_CHOOSE_CODE = 8889;
+
     WebChromeClient webChromeClient = new WebChromeClient() {
+
+        private View videoView;
+        private ViewGroup mContentView;
 
         //=========HTML5定位==========================================================
         //需要先加入权限
@@ -197,7 +227,166 @@ public class Html5Activity extends AppCompatActivity {
             return true;
         }
         //=========多窗口的问题==========================================================
+
+        @Override
+        public void onShowCustomView(View view, CustomViewCallback callback) {
+            super.onShowCustomView(view, callback);
+            mContentView = (ViewGroup) ((ViewGroup) (((Activity) mContext).findViewById(android.R.id.content))).getChildAt(0);
+            mContentView.addView(view);
+            view.setBackgroundColor(Color.BLACK);
+            videoView = view;
+//            oritation = ((Activity)context).getRequestedOrientation();
+//            ((Activity)context).setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        }
+
+        @Override
+        public void onHideCustomView() {
+            super.onHideCustomView();
+            if (videoView != null && mContentView != null) {
+                mContentView.removeView(videoView);
+                videoView = null;
+            }
+//            ((Activity)context).setRequestedOrientation(oritation);
+        }
+
+        protected void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+            mUploadMessage = uploadMsg;
+            fileChooser();
+        }
+
+        protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+            mUploadMessage = uploadMsg;
+            fileChooser();
+        }
+
+        protected void openFileChooser(ValueCallback<Uri> uploadMsg) {
+            mUploadMessage = uploadMsg;
+            fileChooser();
+        }
+
+        @Override
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+            mUploadMessages = filePathCallback;
+            fileChooser();
+            return true;
+        }
     };
+
+    /**
+     * 自定义选择拍照或者相册
+     */
+    private void fileChooser() {
+        PermissionUtil.builder().context(mContext)
+                .permission(Manifest.permission.CAMERA)
+                .permission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .build()
+                .request(new PermissionCallback() {
+                    @Override
+                    public void allow() {
+                        realFileChooser();
+                    }
+
+                    @Override
+                    public void deny(List<String> showDialog) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                            if (mUploadMessages != null) {
+                                mUploadMessages.onReceiveValue(null);
+                                mUploadMessages = null;
+                            }
+                        } else {
+                            if (mUploadMessage != null) {
+                                mUploadMessage.onReceiveValue(null);
+                                mUploadMessage = null;
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void realFileChooser() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(FileUtil.getAppImagesPath(mContext), "webview_temp.png")));
+
+        Intent pickIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        pickIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        pickIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        pickIntent.setType("*/*");
+
+        Intent chooseIntent = new Intent(Intent.ACTION_CHOOSER);
+        chooseIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{cameraIntent});
+        chooseIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
+        chooseIntent.putExtra(Intent.EXTRA_INTENT, pickIntent);
+        ((Activity) mContext).startActivityForResult(chooseIntent, FILE_CHOOSE_CODE);
+    }
+
+    /**
+     * 回调选择文件之后
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode == FILE_CHOOSE_CODE && resultCode == RESULT_OK) {
+                if (mUploadMessages == null)
+                    return;
+                if (data == null) {
+                    File photoFile = new File(FileUtil.getAppImagesPath(mContext), "webview_temp.png");
+                    if (photoFile.exists()) {
+                        Uri fileUri = Uri.fromFile(photoFile);
+                        mUploadMessages.onReceiveValue(new Uri[]{fileUri});
+                        mUploadMessages = null;
+                    }else{
+                        mUploadMessages.onReceiveValue(null);
+                        mUploadMessages = null;
+                    }
+                } else {
+                    ClipData clipData = data.getClipData();
+                    if (clipData != null) {
+                        int itemCount = clipData.getItemCount();
+                        Uri[] uris = new Uri[itemCount];
+                        for (int i = 0; i < itemCount; i++) {
+                            Uri uri = clipData.getItemAt(i).getUri();
+                            uris[i] = uri;
+                        }
+                        mUploadMessages.onReceiveValue(uris);
+                        mUploadMessages = null;
+                    } else {
+                        mUploadMessages.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
+                        mUploadMessages = null;
+                    }
+                }
+            } else {
+                if (mUploadMessages != null) {
+                    mUploadMessages.onReceiveValue(null);
+                    mUploadMessages = null;
+                }
+            }
+        } else if (requestCode == FILE_CHOOSE_CODE && resultCode == RESULT_OK) {
+            if (null == mUploadMessage)
+                return;
+            if (data == null) {
+                File photoFile = new File(FileUtil.getAppImagesPath(mContext), "webview_temp.png");
+                if (photoFile.exists()) {
+                    Uri fileUri = Uri.fromFile(photoFile);
+                    mUploadMessage.onReceiveValue(fileUri);
+                    mUploadMessage = null;
+                } else {
+                    mUploadMessage.onReceiveValue(null);
+                    mUploadMessage = null;
+                }
+            } else {
+                Uri result = data.getData();
+                mUploadMessage.onReceiveValue(result);
+                mUploadMessage = null;
+            }
+        } else {
+            Toast.makeText(mContext, "不支持上传", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -211,18 +400,18 @@ public class Html5Activity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
-
         if (mWebView != null) {
+            mWebView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
             mWebView.clearHistory();
+
             ((ViewGroup) mWebView.getParent()).removeView(mWebView);
-            mWebView.loadUrl("about:blank");
             mWebView.stopLoading();
             mWebView.setWebChromeClient(null);
             mWebView.setWebViewClient(null);
             mWebView.destroy();
             mWebView = null;
         }
+        super.onDestroy();
     }
 
 }
